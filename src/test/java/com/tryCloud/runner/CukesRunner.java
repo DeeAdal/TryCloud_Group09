6a8647383b7a0e9e456556ad3b1f12a4afb60723
Derege Adal <dereges@yahoo.com>
2022-11-03 13:30:16 -0400

    tryCloud created main branch

---
+++ src/test/java/com/tryCloud/runner/CukesRunner.java
@@ -0,0 +1,21 @@
+package com.tryCloud.runner;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {"html:target/cucumber-report.html",
+                "rerun:target/rerun.txt",
+                "me.jvt.cucumber.report.PrettyReports:target/cucumber"
+                },
+        features = "src/test/resources/features",
+        glue = "src/test/java/com/tryCloud/step_defs",
+        dryRun = false,
+        tags = ""
+)
+
+public class CukesRunner {
+
+}
