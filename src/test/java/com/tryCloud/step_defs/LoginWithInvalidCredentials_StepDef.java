1ace85c8a450737bd5ae76d945bd0bfc6471c68d
Soelma5 <sanzhieva2020@gmail.com>
2022-11-07 11:39:11 -0500

    Log in with invalid credentials

---
+++ src/test/java/com/tryCloud/step_defs/LoginWithInvalidCredentials_StepDef.java
@@ -0,0 +1,36 @@
+package com.tryCloud.step_defs;
+
+import com.tryCloud.pages.LoginWithInvalidCredentialsPage;
+import com.tryCloud.utilis.ConfigurationReader;
+import com.tryCloud.utilis.Driver;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.junit.Assert;
+
+public class LoginWithInvalidCredentials_StepDef {
+    LoginWithInvalidCredentialsPage loginWithInvalidCredentialsPage = new LoginWithInvalidCredentialsPage();
+
+    @Given("user on the TryCloud login page")
+    public void user_on_the_try_cloud_login_page() {
+
+        Driver.getDriver().get("http://qa3.trycloud.net/index.php/login?clear=1");
+    }
+
+    @When("user enters {string} and enters {string}")
+    public void user_enters_and_enters(String string, String string2) {
+        loginWithInvalidCredentialsPage.username.sendKeys(string);
+        loginWithInvalidCredentialsPage.password.sendKeys(string2);
+    }
+
+    @When("user clicks on login button")
+    public void user_clicks_on_login_button() {
+    loginWithInvalidCredentialsPage.loginBtn.click();
+    }
+
+    @Then("verify {string} message should be displayed")
+    public void verify_message_should_be_displayed(String string) {
+    Assert.assertEquals(string, loginWithInvalidCredentialsPage.failedMsg.getText());
+    }
+
+}
