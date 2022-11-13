0abb76774dabc96d8e1c94c4a7e386328f108f31
Salma Sali <salianovaj@gmail.com>
2022-11-13 06:10:08 -0500

    - instance for Explicit wait was initiated to handle synchronization issues - hover() was used from BrowserUtils to make actonIcon visible for Driver

--- src/test/java/com/tryCloud/step_defs/Comments_StepDef.java
+++ src/test/java/com/tryCloud/step_defs/Comments_StepDef.java
@@ -2,37 +2,40 @@ package com.tryCloud.step_defs;
 
 import com.tryCloud.pages.CommentsPage;
 import com.tryCloud.utilis.BrowserUtils;
-import com.tryCloud.utilis.ConfigurationReader;
 import com.tryCloud.utilis.Driver;
 import io.cucumber.java.en.And;
-import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-import org.junit.Assert;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class Comments_StepDef {
 
-CommentsPage commentsPage = new CommentsPage();
+    CommentsPage commentsPage = new CommentsPage();
+    WebDriverWait wait = new WebDriverWait(Driver.getDriver(), 30);
 
     @When("user click action-icon  from any file on the page")
     public void user_click_action_icon_from_any_file_on_the_page() {
-         commentsPage.allFilesBtn.click();
-         commentsPage.actionIcon.click();
+        wait.until(ExpectedConditions.visibilityOf(commentsPage.fileSummary));
+        BrowserUtils.hover(Driver.getDriver().findElement(By.xpath("(//a[@class='action action-menu permanent'])[1]")));
+        commentsPage.actionIcon.click();
 
     }
 
     @And("user choose the {string} option")
     public void userChooseTheOption(String arg) {
-     commentsPage.detailsOption.click();
         BrowserUtils.sleep(2);
+        commentsPage.detailsOption.click();
+
     }
 
 
     @When("user write a comment inside the input box")
     public void user_write_a_comment_inside_the_input_box() {
+        wait.until(ExpectedConditions.visibilityOf(commentsPage.commentButton));
         commentsPage.commentButton.click();
+        wait.until(ExpectedConditions.visibilityOf(commentsPage.newComment));
         commentsPage.newComment.click();
         commentsPage.newComment.sendKeys("good job");
 
@@ -54,8 +57,7 @@ CommentsPage commentsPage = new CommentsPage();
     }
 
 
-
-    }
+}
 
 
 
